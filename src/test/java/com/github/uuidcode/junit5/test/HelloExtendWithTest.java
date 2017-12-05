a2d6b7d3d64aacd38185666ee673bb80668a8990
uuidcode <uuidcode@gmail.com>
2017-12-05 16:54:42 +0900

    HelloMethodExtension

--- src/test/java/com/github/uuidcode/junit5/test/HelloExtendWithTest.java
+++ src/test/java/com/github/uuidcode/junit5/test/HelloExtendWithTest.java
@@ -1,12 +1,24 @@
 package com.github.uuidcode.junit5.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(HelloExtension.class)
 public class HelloExtendWithTest {
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("-->");
+    }
+
     @Test
-    public void test() {
-        System.out.println("test");
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    @ExtendWith(HelloMethodExtension.class)
+    public void test2() {
+        System.out.println("test2");
     }
 }
