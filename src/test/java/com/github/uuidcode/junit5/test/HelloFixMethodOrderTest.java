bee19976965647b89bbe99e6341afaefe9b8ee4f
uuidcode <uuidcode@gmail.com>
2017-12-08 19:12:03 +0900

    HelloFixMethodOrderTest

---
+++ src/test/java/com/github/uuidcode/junit5/test/HelloFixMethodOrderTest.java
@@ -0,0 +1,23 @@
+package com.github.uuidcode.junit5.test;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class HelloFixMethodOrderTest {
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+    }
+}
